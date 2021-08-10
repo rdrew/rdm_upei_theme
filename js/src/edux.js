b55c400fc1b42a1b95d042e38cfcf9420d139e80
rdrew <rdrew@upei.ca>
2021-08-10 13:55:10 -0300

    accordion block js

--- js/src/edux.js
+++ js/src/edux.js
@@ -3,6 +3,10 @@
 jQuery(document).ready(function ($) {
   // Homepage blocks
   $(".region-content-home-top .block, .region-content-home-bottom .block").wrapInner( '<div class="container"></div>' );
+
+  $('#accordion').accordionjs({
+    activeIndex: false,
+  });
 /* End document
 --------------------------*/
 });
